79769c5758959a9b86fdedaf4b2a60e111b1fc18
Sviatoslav_Taranenko <sviat969@icloud.com>
2023-10-26 21:05:28 +0300

    asdasd

--- script/script.js
+++ script/script.js
@@ -1,13 +1,1 @@
-const usdCurr = 28;
-const discount = 0.9;
 
-function convert(amount, curr) {
-    return curr * amount;
-
-}
-
-function prmotion(result) {
-    console.log(result * discount);
-}
-
-prmotion(convert(500, usdCurr));
