55b31cac0f72e07b97c827930fcc2fb9bae4df25
Sviatoslav Taranenko <sviatoslav.taranenko@uf.ua>
2023-10-27 11:34:40 +0300

    ASda

--- Example for code/script/script.js
+++ Example for code/script/script.js
@@ -1,4 +1,38 @@
-let title = document.getElementById("title");
+/* let title = "Basic math operation"
+console.log(title);
+
+let firstNumber = 20;
+let secondNumber = 30;
+
+let result1 = firstNumber + secondNumber;
+
+console.log('Operation addition:', firstNumber, '+', secondNumber, '=', result1);
+
+let result2 = firstNumber - secondNumber;
+
+console.log('Operation addition:', firstNumber, '-', secondNumber, '=', result2);
+
+let result3 = firstNumber * secondNumber;
+
+console.log('Operation addition:', firstNumber, '*', secondNumber, '=', result3);
+
+let result4 = firstNumber / secondNumber;
+
+console.log('Operation addition:', firstNumber, '/', secondNumber, '=', result4);
+
+let result5 = firstNumber % secondNumber;
+
+console.log('Operation addition:', firstNumber, '%', secondNumber, '=', result5);
+
+let result6 = firstNumber ** secondNumber;
+
+console.log('Operation addition:', firstNumber, '**', secondNumber, '=', result6); */
+
+
+
+
+
+let container = document.getElementById("container");
 let newElement = document.createElement("h1");
-    newElement.textContent = "Basic math operation";
+    newElement.textContent = "Новий елемент створений з JavaScript";
     container.appendChild(newElement);
