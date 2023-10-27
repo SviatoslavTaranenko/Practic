7c7fc0b1c5d42dabb803d038c0f9643534c13f69
Sviatoslav Taranenko <sviatoslav.taranenko@uf.ua>
2023-10-27 09:41:17 +0300

    Add file for examples

---
+++ Example for code/script/script.js
@@ -0,0 +1,4 @@
+let title = document.getElementById("title");
+let newElement = document.createElement("h1");
+    newElement.textContent = "Basic math operation";
+    container.appendChild(newElement);
